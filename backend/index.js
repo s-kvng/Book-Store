6f55fd5da142971fdc1a120227dbbb4d97e4e238
s-kvng <kvngnathan8420@gmail.com>
2024-01-04 23:58:29 +0000

    update index

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,17 @@ const mongoose = require("./db");
 const { Book } = require("./models/bookModel");
 const app = express();
 
+//middleware for parsing json body
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.status(200).send("Welcome to the MERN Tutorail");
 });
 
-app.post("/book", async (req, res) => {
+app.post("/books", async (req, res) => {
   try {
     const { title, author, publishYear } = req.body;
+    console.log(`${title} - ${author} - ${publishYear}`);
     if (!title || !author || !publishYear) {
       return res.status(400).send({
         success: false,
@@ -25,7 +29,10 @@ app.post("/book", async (req, res) => {
       publishYear: publishYear,
     };
 
+    console.log(newBook);
     const book = await Book.create(newBook);
+
+    return res.status(201).send({ success: true, data: book });
   } catch (error) {
     console.log(error.message);
     return res
