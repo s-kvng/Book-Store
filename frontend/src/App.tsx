8641f4c0143722a262226e4e3d1e738e0e29e92e
s-kvng <kvngnathan8420@gmail.com>
2024-01-11 02:13:27 +0000

    setup tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,9 @@ function App() {
   return (
     <>
       <div>
+      <h1 className="text-5xl font-bold underline text-pink-600 ">
+      Hello world!
+    </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
