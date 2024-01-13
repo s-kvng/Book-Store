87c82c1257045f05cb021b8856fb19b0a464c331
s-kvng <kvngnathan8420@gmail.com>
2024-01-13 00:43:02 +0000

    fetch data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,24 @@
-
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 const Home = () => {
+  const [books, setBooks] = useState([])
+  const [isLoading , setIsLoading] = useState(false)
+
+  useEffect(()=>{
+
+    setIsLoading(true)
+    axios.get('')
+    .then((res)=>{
+      console.log(res.data)
+    })
+    .catch((err)=>{
+      console.log(err.message)
+    })
+
+
+  }, [])
+
   return (
     <div>Home</div>
   )
