d94056e4519b0571eb9f31a784fb4b4ef01e1e06
s-kvng <kvngnathan8420@gmail.com>
2024-01-15 21:02:30 +0000

    update home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,16 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 
+import Button from "../components/Button"
+
 const Home = () => {
-  const [books, setBooks] = useState([])
-  const [isLoading , setIsLoading] = useState(false)
+  const [books, setBooks] = useState<Object[]>([])
+  const [isLoading , setIsLoading] = useState<boolean>(false)
 
   useEffect(()=>{
 
     setIsLoading(true)
-    axios.get('http//:localhost:5172')
+    axios.get('http//:localhost:5173')
     .then((res)=>{
       console.log(res.data)
     })
@@ -20,7 +22,14 @@ const Home = () => {
   }, [])
 
   return (
-    <div>Home</div>
+    <div className="  p-5">
+      <div className="flex justify-between items-center my-5">
+        <h1>Books List</h1>
+        <div className="">
+        <Button className=" bg-sky-600 hover:bg-sky-300 text-xl" name="Table"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
