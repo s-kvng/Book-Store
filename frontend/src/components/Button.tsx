9cfe57c8ef51474ca8fabecfac050e2fb8faed05
s-kvng <kvngnathan8420@gmail.com>
2024-01-20 19:45:31 +0000

    update delete page

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 type Props =   {
     className: string,
     name: string,
-    types: "button" | "reset" | "submit" | undefined
+    types: "button" | "reset" | "submit" | undefined,
+    onClick?: ()=> void
 }
 
-const Button: React.FC<Props>  = ({ className = "" , name , types}) => {
+const Button: React.FC<Props>  = ({ className = "" , name , types, onClick}) => {
   return (
-    <button type={types}  className={`border-none rounded-lg px-5 py-3 cursor-pointer ` + className} >{name}</button>
+    <button onClick={onClick} type={types}  className={`border-none rounded-lg px-5 py-3 cursor-pointer ` + className} >{name}</button>
   )
 }
 
--- frontend/src/pages/DeleteBook.tsx
+++ frontend/src/pages/DeleteBook.tsx
@@ -1,8 +1,52 @@
+import { useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import axios from "axios"
+
+import Spinner from "../components/Spinner"
+import BackButton from "../components/BackButton"
+import Button from "../components/Button"
 
 
 const DeleteBook = () => {
+const navigate = useNavigate()
+const {id} = useParams()
+const [ isLoading , setIsLoading] = useState<boolean>(false)
+
+const handleDelete = (e: React.FormEvent): void => {
+  e.preventDefault()
+  setIsLoading(true)
+
+  axios.delete(`http://localhost:5173/books/${id}/`)
+  .then(()=>{
+    setIsLoading(false)
+    navigate("/")
+  })
+  .catch(()=>{
+    alert("Could not delete book!")
+    setIsLoading(false)
+  })
+}
+
+
   return (
-    <div>DeleteBook</div>
+    <div className="p-5">
+      <div className=" mb-10">
+        <BackButton destination="/"/>
+        <h1 className=" text-2xl my-2">Delete Book</h1>
+      </div>
+      {isLoading ? 
+        <div  className="flex justify-center items-center pt-16">
+        <Spinner/>
+        </div>
+        :
+        <div className="flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center p-8 shadow-xl hover:shadow-lg  gap-y-6 w-full sm:w-1/2 bg-gray-200 rounded-lg">
+            <h1 className=" text-2xl font-semibold">Are you sure you want to delete this book ? </h1>
+            <Button onClick={()=> handleDelete } types="button" className="bg-red-600 hover:bg-red-500" name="Delete"/>
+          </div>
+        </div>   
+      }
+    </div>  
   )
 }
 
